69751b7304c9a73b07929ed9bc062234996b4465
Vinicius <vinicius_cok@hotmail.com>
2018-07-11 01:18:58 -0300

    vinicius-11-07-2018

---
+++ PROJETO/Arquivo/src/main/java/br/com/brasfrut/ArquivoApplication.java
@@ -0,0 +1,12 @@
+package br.com.brasfrut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquivoApplication.class, args);
+	}
+}
